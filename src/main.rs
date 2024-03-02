ad1b22b594d9f57988f2b28fed587d32d2f0d870
PintaPontus <matteo.blascoviz@gmail.com>
2024-03-02 15:55:22 +0100

    feat(command): message validation

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,7 @@ async fn answer(bot: Bot, msg: Message, cmd: SerbenCommand) -> ResponseResult<()
     match cmd {
         SerbenCommand::Help => {
             bot.send_message(msg.chat.id, SerbenCommand::descriptions().to_string())
-                .await?
+                .await?;
         }
         SerbenCommand::Ip => {
             bot.send_message(
@@ -46,7 +46,7 @@ async fn answer(bot: Bot, msg: Message, cmd: SerbenCommand) -> ResponseResult<()
                     .get("ip")
                     .unwrap_or(&String::from("Impossibile reperire l'ip")),
             )
-            .await?
+            .await?;
         }
         SerbenCommand::Accendi => match serben_start() {
             Ok(output) => {
@@ -54,14 +54,14 @@ async fn answer(bot: Bot, msg: Message, cmd: SerbenCommand) -> ResponseResult<()
                     msg.chat.id,
                     format!("Accensione: {}", String::from_utf8(output.stdout).unwrap()),
                 )
-                .await?
+                .await?;
             }
             Err(error) => {
                 bot.send_message(
                     msg.chat.id,
                     format!("Impossibile avviare il Serben:\n{error}"),
                 )
-                .await?
+                .await?;
             }
         },
         SerbenCommand::Spegni => match serben_stop() {
@@ -70,14 +70,14 @@ async fn answer(bot: Bot, msg: Message, cmd: SerbenCommand) -> ResponseResult<()
                     msg.chat.id,
                     format!("Spegnimento: {}", String::from_utf8(output.stdout).unwrap()),
                 )
-                .await?
+                .await?;
             }
             Err(error) => {
                 bot.send_message(
                     msg.chat.id,
                     format!("Impossibile fermare il Serben:\n{error}"),
                 )
-                .await?
+                .await?;
             }
         },
         SerbenCommand::Logs(lines) => {
@@ -86,30 +86,30 @@ async fn answer(bot: Bot, msg: Message, cmd: SerbenCommand) -> ResponseResult<()
                     msg.chat.id,
                     "Inserire un numero valido di linee".to_string(),
                 )
-                .await?
-            }
-            if lines > 30 {
+                .await?;
+            } else if lines > 30 {
                 bot.send_message(
                     msg.chat.id,
                     "Impossibile inviare più di 30 linee".to_string(),
                 )
-                .await?
-            }
-            match serben_logs(lines) {
-                Ok(logs) => {
-                    let logs_lines = String::from_utf8(logs.stdout).unwrap();
-                    bot.send_message(
-                        msg.chat.id,
-                        format!("Ultime {} linee di logs:\n```{}```", lines, logs_lines),
-                    )
-                    .await?
-                }
-                Err(error) => {
-                    bot.send_message(
-                        msg.chat.id,
-                        format!("Impossibile recuperare i log:\n{error}"),
-                    )
-                    .await?
+                .await?;
+            } else {
+                match serben_logs(lines) {
+                    Ok(logs) => {
+                        let logs_lines = String::from_utf8(logs.stdout).unwrap();
+                        bot.send_message(
+                            msg.chat.id,
+                            format!("Ultime {} linee di logs:\n```{}```", lines, logs_lines),
+                        )
+                        .await?;
+                    }
+                    Err(error) => {
+                        bot.send_message(
+                            msg.chat.id,
+                            format!("Impossibile recuperare i log:\n{error}"),
+                        )
+                        .await?;
+                    }
                 }
             }
         }
@@ -119,7 +119,7 @@ async fn answer(bot: Bot, msg: Message, cmd: SerbenCommand) -> ResponseResult<()
                     msg.chat.id,
                     "Inserire un numero valido di minuti".to_string(),
                 )
-                .await?
+                .await?;
             }
             let min_word = if minutes > 0 { "minuti" } else { "minuto" };
             match erobren_shutdown(minutes) {
@@ -128,14 +128,14 @@ async fn answer(bot: Bot, msg: Message, cmd: SerbenCommand) -> ResponseResult<()
                         msg.chat.id,
                         format!("Spegnimento in {minutes} {min_word}🛑"),
                     )
-                    .await?
+                    .await?;
                 }
                 Err(error) => {
                     bot.send_message(
                         msg.chat.id,
                         format!("Impossibile spegnere il server:\n{error}"),
                     )
-                    .await?
+                    .await?;
                 }
             }
         }
@@ -163,22 +163,20 @@ fn serben_logs(lines: i32) -> std::io::Result<Output> {
 
 #[cfg(target_os = "windows")]
 fn erobren_shutdown(minutes: i32) -> std::io::Result<Output> {
-    Command::new("shutdown")
-        .args(if minutes > 0 {
-            ["-s", "-t", &(minutes * 60).to_string(), "-f"]
-        } else {
-            ["-s", "-p", "-f"]
-        })
-        .output()
+    let args = if minutes > 0 {
+        [format!("-s -t {} -f", minutes * 60)]
+    } else {
+        ["-s -p -f".to_string()]
+    };
+    Command::new("shutdown").args(args).output()
 }
 
 #[cfg(not(target_os = "windows"))]
 fn erobren_shutdown(minutes: i32) -> std::io::Result<Output> {
-    Command::new("shutdown")
-        .args(if minutes > 0 {
-            ["-P", format!("+{seconds}").as_str()]
-        } else {
-            ["-h", "now"]
-        })
-        .output()
+    let args = if minutes > 0 {
+        [format!("-P +{seconds}")]
+    } else {
+        ["-h now".to_string()]
+    };
+    Command::new("shutdown").args(args).output()
 }
